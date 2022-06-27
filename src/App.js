c78338d46e2579de87458841fc878bbb1577d387
Vikram Kumar <81801195+mrpvikramkumar@users.noreply.github.com>
2022-06-27 23:38:57 +0530

    Add files via upload
    Todo list that adds data in firebase(database)

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/usehttps';
+import Footer from './Footer/Footer'
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-http-683cd-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+      <Footer/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; Vikram Kumar | 2022</p>
+    </footer>
+  );
+};
+
+export default Footer;
